b49f020869a9e849d7153cf5b46663be6db7810d
Douglas Silva <doug.neo@gmail.com>
2024-04-11 22:57:14 -0300

    Adicionando Env ao sistema

--- lib/client.ex
+++ lib/client.ex
@@ -2,11 +2,10 @@ defmodule Client do
   use Tesla
 
   plug Tesla.Middleware.BaseUrl, "https://api.github.com"
-  # preciso passar no header um user-agent
-  plug Tesla.Middleware.Headers, [{"authorization", "TOKEN HERE"}, {"user-agent", "Elixir"}]
+  plug Tesla.Middleware.Headers, [{"authorization", "#{System.env()}"}, {"user-agent", "Elixir"}]
   plug Tesla.Middleware.JSON
 
   def user_repos(id) do
-    get("rota aqui")
+    get()
   end
 end

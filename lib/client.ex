5e0c32c6ee8f55e9872693ba7ffed72f741faf15
Douglas Silva <doug.neo@gmail.com>
2024-04-11 23:33:49 -0300

    Ajustando as consulta

--- lib/client.ex
+++ lib/client.ex
@@ -2,10 +2,18 @@ defmodule Client do
   use Tesla
 
   plug Tesla.Middleware.BaseUrl, "https://api.github.com"
-  plug Tesla.Middleware.Headers, [{"authorization", "#{System.env()}"}, {"user-agent", "Elixir"}]
+  plug Tesla.Middleware.Headers, [{"authorization", "#{System.env(GIT_TOKEN)}"}, {"user-agent", "Elixir"}]
   plug Tesla.Middleware.JSON
 
-  def user_repos(id) do
-    get()
-  end
+    # Endpoint genérico para fazer solicitações GET
+    def get(endpoint), do: Tesla.get(endpoint)
+
+    # Endpoint genérico para fazer solicitações POST
+    def post(endpoint, body \\ %{}), do: Tesla.post(endpoint, body)
+
+    # Endpoint genérico para fazer solicitações PATCH
+    def patch(endpoint, body \\ %{}), do: Tesla.patch(endpoint, body)
+
+    # Endpoint genérico para fazer solicitações DELETE
+    def delete(endpoint), do: Tesla.delete(endpoint)
 end
